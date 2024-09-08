11b2b78a401de7bdde390508a1f1f1d1e419ce75
CoreOfChaos <157353490+AdamCofala@users.noreply.github.com>
2024-09-08 14:44:14 +0200

    Update WebsiteGenerator.cpp
    user input

--- WebsiteGenerator.cpp
+++ WebsiteGenerator.cpp
@@ -155,12 +155,128 @@ void generateHTML(ofstream &htmlFile, const vector<string> &data) {
     htmlFile << "</body>\n</html>\n";
 }
 
-int main() {
-    // Otwieranie pliku wejściowego
-    ifstream inputFile("dane.txt");
-    vector<string> data;
-    string line;
+void dataUserInput(vector<string> &data) {
+    string input;
 
+    // 1. Kolor tla strony
+    cout << "Podaj kolor tla (np. #000000 dla czarnego lub nazwe koloru): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 2. Obraz tla strony
+    cout << "Podaj sciezke do obrazu tla (lub wpisz 'NIE' jesli brak): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 3. Opcje tla strony (np. RY_TAK, ROZ_TAK, RX_TAK, FIX_TAK)
+    cout << "Podaj opcje tla strony (np. RY_TAK;ROZ_TAK lub 'NIE' jesli brak): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 4. Tytul strony
+    cout << "Podaj tytul strony: ";
+    cin.ignore();  // Oczyszczenie bufora po wczesniejszym cin
+    getline(cin, input);
+    data.push_back(input);
+
+    // 5. Rozmiar czcionki tytulu
+    cout << "Podaj rozmiar czcionki tytulu (w px): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 6. Kolor czcionki tytulu
+    cout << "Podaj kolor czcionki tytulu: ";
+    cin >> input;
+    data.push_back(input);
+
+    // 7. Liczba kolumn
+    cout << "Podaj liczbe kolumn: ";
+    cin >> input;
+    data.push_back(input);
+
+    // 8. Teksty kolumn (oddzielone srednikami)
+    cout << "Podaj teksty kolumn oddzielone srednikami (np. 'Tekst1;Tekst2;Tekst3'): ";
+    cin.ignore();  // Oczyszczenie bufora
+    getline(cin, input);
+    data.push_back(input);
+
+    // 9. Rozmiary czcionek kolumn (oddzielone srednikami)
+    cout << "Podaj rozmiary czcionek kolumn oddzielone srednikami (np. '12;14;16'): ";
+    getline(cin, input);
+    data.push_back(input);
+
+    // 10. Kolory czcionek kolumn (oddzielone srednikami)
+    cout << "Podaj kolory czcionek kolumn oddzielone srednikami (np. 'red;blue;green'): ";
+    getline(cin, input);
+    data.push_back(input);
+
+    // 11. Kolory tla kolumn (oddzielone srednikami)
+    cout << "Podaj kolory tla kolumn oddzielone srednikami (np. '#ffffff;#000000;#ff0000'): ";
+    getline(cin, input);
+    data.push_back(input);
+
+    // 12. Obrazy tla kolumn (oddzielone srednikami, wpisz 'NIE' jesli nie ma)
+    cout << "Podaj obrazy tla kolumn oddzielone srednikami (wpisz 'NIE', jesli brak): ";
+    getline(cin, input);
+    data.push_back(input);
+
+    // 13. Opcje tla kolumn (oddzielone srednikami, np. 'RY_TAK;RX_TAK')
+    cout << "Podaj opcje tla kolumn oddzielone srednikami (np. 'RY_TAK;RX_TAK'): ";
+    getline(cin, input);
+    data.push_back(input);
+
+    // 14. Zaokraglenie rogow kolumn (w px)
+    cout << "Podaj wartosc zaokraglenia rogow kolumn (w px): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 15. Kolor tla stopki
+    cout << "Podaj kolor tla stopki: ";
+    cin >> input;
+    data.push_back(input);
+
+    // 16. Obraz tla stopki
+    cout << "Podaj sciezke do obrazu tla stopki (lub wpisz 'NIE' jesli brak): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 17. Opcje tla stopki (np. 'RY_TAK;ROZ_TAK')
+    cout << "Podaj opcje tla stopki (np. 'RY_TAK;ROZ_TAK'): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 18. Kolor czcionki stopki
+    cout << "Podaj kolor czcionki stopki: ";
+    cin >> input;
+    data.push_back(input);
+
+    // 19. Rozmiar czcionki stopki (w px)
+    cout << "Podaj rozmiar czcionki stopki (w px): ";
+    cin >> input;
+    data.push_back(input);
+
+    // 20. Tekst stopki
+    cout << "Podaj tekst stopki: ";
+    cin.ignore();
+    getline(cin, input);
+    data.push_back(input);
+}
+
+
+
+int main() {
+   
+    vector<string> data;  //vector w którym będą umieszczane wszystkie dane
+
+    cout << "Czy chcesz wczytac dane z pliku dane.txt? wpisz 't' w przeciwnym razie wpisz 'n' : ";
+    char odp;
+    cin>>odp;
+
+    if(odp=='t'){
+
+    ifstream inputFile("dane.txt");
+    string line;
+    // Otwieranie pliku wejściowego
     if (inputFile.is_open()) {
         while (getline(inputFile, line)) {
             data.push_back(line);
@@ -169,6 +285,10 @@ int main() {
     } else {
         cerr << "Nie można otworzyć pliku dane.txt" << endl;
         return 1;
+    }
+
+    }else{
+        dataUserInput(data);
     }
 
     // Otwieranie plików wyjściowych
